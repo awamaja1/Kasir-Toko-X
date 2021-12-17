f9caf18eeea84878c6c751e0458f7ba92dcd4ffe
Rafi Nouval <60467525+awamaja1@users.noreply.github.com>
2021-12-17 21:40:21 +0700

    Update Additional.java
    tambah fitur untuk generate string random

--- Additional.java
+++ Additional.java
@@ -3,6 +3,26 @@ import java.text.DecimalFormatSymbols;
 
 public class Additional {
 
+	private StringBuilder stringBuilder = new StringBuilder();
+    	private Random random = new Random();
+    
+
+    	public String amblKdAck() {
+    		char[] karakter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".toCharArray();
+    		String hasil;
+    	
+		for (int lenght = 0; lenght < 7; lenght++) {
+		    Character character = karakter[random.nextInt(karakter.length)];
+		    stringBuilder.append(character);
+		}
+
+		hasil = stringBuilder.toString();
+		stringBuilder.delete(0, 7);
+
+		return hasil;
+	}
+	
+	
 	public void kursRp(Double angka) {
 		DecimalFormat kursIndonesia = (DecimalFormat) DecimalFormat.getCurrencyInstance();
 		DecimalFormatSymbols formatRp = new DecimalFormatSymbols();
