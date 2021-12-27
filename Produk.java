aaff93f0b1c8925e283735bc1cba95d95bb06895
Rafi Nouval <60467525+awamaja1@users.noreply.github.com>
2021-12-28 06:38:47 +0700

    Update Produk.java

--- Produk.java
+++ Produk.java
@@ -1,50 +1,60 @@
+package aplikasiKasir;
 
-public class Produk {
+class Produk {
 
-	private static String[][] mProduk = {
-			{"(1)", "Susu UHT", "20000", "0"},
-			{"(2)", "Mie Goreng", "2500", "0"},
-			{"(3)", "Kopi Kapal Api 380g", "23000", "0"},
-			{"(4)", "Chocolate Silver Queen", "18000", "0"},
-			{"(5)", "Es Krim", "5000", "0"}
-	};
+	private String nama;
+	private double harga;
+	private int stokTerjual;
+	private int pnjlStokPerTrnsksi;
 	
-	public String[][] listMnplt = mProduk;
-	private String[][] backupList = mProduk;
-	
-	String namaProduk;
-	double hargaProduk;
-	int stokAwal;
-	
-	public String[][] tabelProduk() {
-		return listMnplt;
+	public Produk(String nama, double harga) {
+		super();
+		this.nama = nama;
+		this.harga = harga;
+		this.stokTerjual = 0;
+	}
+
+	public String getNama() {
+		return nama;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+
+	public double getHarga() {
+		return harga;
+	}
+
+	public void setHarga(double harga) {
+		this.harga = harga;
+	}
+
+	public int getStokTerjual() {
+		return stokTerjual;
+	}
+
+	public void setStokTerjual(int stokTerjual) {
+		this.stokTerjual = stokTerjual;
 	}
 	
-	public void pencarianProduk(int indeks) {
-		
-		if (indeks > 0 && indeks <= listMnplt.length) {
-			namaProduk = listMnplt[indeks-1][1];
-			hargaProduk = Double.parseDouble(listMnplt[indeks-1][2]);
-			stokAwal = Integer.parseInt(listMnplt[indeks-1][3]);
-		}
-		
+	public void sumStokTerjual(int tambahTerjual) {
+		this.stokTerjual += tambahTerjual;
 	}
 	
-	public void tmbhPenjualanBrng(int indeks, int stokTerjual) {
-		
-		listMnplt[indeks-1][3] = String.valueOf(stokAwal + stokTerjual);
-		
+	public void minStokTerjual(int stokTerjual) {
+		this.stokTerjual -= stokTerjual;
 	}
 	
-	public void pengembalianStok() {
-		listMnplt = backupList;
+	public void setCatatanStok(int catatanStok) {	
+		this.pnjlStokPerTrnsksi = catatanStok;
 	}
 	
-	public void konfirmasiStok() {
-		backupList = listMnplt;
+	public int getCatatanStok() {
+		return this.pnjlStokPerTrnsksi;
 	}
 	
-	public void kmbliKeAwal() {
-		listMnplt = mProduk;
+	public double getPendapatanStok() {
+		return this.harga * this.stokTerjual;
 	}
 }
