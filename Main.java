a063af638def28fce4d627de682d421c00319ceb
Rafi Nouval <60467525+awamaja1@users.noreply.github.com>
2021-12-28 06:39:42 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package aplikasiKasir;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		Kasir kasir = new Kasir();
+		kasir.main();
+	}
+
+}
